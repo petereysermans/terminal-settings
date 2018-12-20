7ab85d813b4ff788b1a28260eee23ac8b6436cc3
Peter Eysermans <peter@eysermans.com>
2018-12-20 11:49:27 +0100

    Added cmder aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,14 @@
-alias gac="git add . and git commit -v"
+# GIT aliases
+alias gac="git add .; and git commit -v"
+alias gs="git status"
+
+alias gp="git push"
+alias gpo="git push origin"
+alias gpom="git push origin master"
+
+alias gl="git pull"
+alias glo="git pull origin"
+alias glom="git pull origin master"
+
+alias gb="git branch"
+alias gc="git checkout"
