aa8377624877cc56cbb8afd340c950f5ac84c54b
Peter Eysermans <peter@eysermans.com>
2019-06-05 12:14:03 +0200

    Add --follow-tags flag to the push/pull command aliases

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,13 @@
 alias gac="git add -A; and git commit -v"
 alias gs="git status"
 
-alias gp="git push --tags"
-alias gpo="git push --tags origin"
-alias gpom="git push --tags origin master"
+alias gp="git push --follow-tags"
+alias gpo="git push origin --follow-tags"
+alias gpom="git push origin master --follow-tags"
 
-alias gl="git pull"
-alias glo="git pull origin"
-alias glom="git pull origin master"
+alias gl="git pull --follow-tags"
+alias glo="git pull origin --follow-tags"
+alias glom="git pull origin master --follow-tags"
 
 alias gb="git branch"
 alias gc="git checkout"
