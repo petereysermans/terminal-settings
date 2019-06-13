c3355c78e3727fcbed4e36631559c16ceb44e9e5
Peter Eysermans <peter@eysermans.com>
2019-06-13 12:01:50 +0200

    Removed --follow-tags flag from git pull aliases

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,13 @@
 alias gac="git add -A; and git commit -v"
 alias gs="git status"
 
-alias gp="git push --follow-tags"
+alias gp="git push"
 alias gpo="git push origin --follow-tags"
 alias gpom="git push origin master --follow-tags"
 
-alias gl="git pull --follow-tags"
-alias glo="git pull origin --follow-tags"
-alias glom="git pull origin master --follow-tags"
+alias gl="git pull"
+alias glo="git pull origin"
+alias glom="git pull origin master"
 
 alias gb="git branch"
 alias gc="git checkout"
